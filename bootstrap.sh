eaf4c9e8fd0cbe099f85cfd015eb5312f2a3fb08
Jonas <purtsjo@gmail.com>
2020-09-22 21:20:35 +0200

    Fix shell PlugInstall

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ stow zsh
 mkdir -p ~/.vim/autoload
 curl -fLo ~/dotfiles/vim/.vim/autoload/plug.vim \
   --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c 'PlugInstall' -c 'qa!'
+vim +'PlugInstall --sync' +'qa!'
 stow vim
 
 # Tmux:
