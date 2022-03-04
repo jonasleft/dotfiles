51bc8e68b84887f76ca5c2bd60d4610658e5544b
Jonas <purtsjo@gmail.com>
2022-03-05 00:46:01 +0100

    copy terminal config

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -10,5 +10,8 @@ cp -v xubuntu/$cfgpath/*.xml ../$cfgpath/
 cfgpath=.config/autostart
 cp -v xubuntu/$cfgpath/* ../$cfgpath/
 
+cfgpath=.config/xfce4/terminal
+cp -v xubuntu/$cfgpath/* ../$cfgpath/
+
 
 
