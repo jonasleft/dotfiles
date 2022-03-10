742c26821e437bde55b18bb01b119484fe3ea55a
JonasP <purtsjo@gmail.com>
2022-03-10 17:30:59 +0100

    ubuntu setup: owncloud

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,6 +4,8 @@
 
 sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
 
+## XFCE settings:
+
 cfgpath=.config/xfce4/xfconf/xfce-perchannel-xml
 cp -v xubuntu/$cfgpath/*.xml ../$cfgpath/
 
@@ -13,5 +15,12 @@ cp -v xubuntu/$cfgpath/* ../$cfgpath/
 cfgpath=.config/xfce4/terminal
 cp -v xubuntu/$cfgpath/* ../$cfgpath/
 
+
+## Owncloud:
+
+wget -nv https://download.owncloud.com/desktop/ownCloud/stable/2.10/linux/Ubuntu_21.10/Release.key -O - | sudo apt-key add -
+echo 'deb https://download.owncloud.com/desktop/ownCloud/stable/2.10/linux/Ubuntu_21.10/ /' | sudo tee -a /etc/apt/sources.list.d/owncloud.list
+sudo apt update
+sudo apt install owncloud-client
 
 
