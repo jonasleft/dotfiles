580b1ee8a9addad8e7230990ed859fcdd7b322f9
jonasleft <purtsjo@gmail.com>
2022-09-08 21:58:53 +0200

    install git

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 # run with:  
 #   curl https://raw.githubusercontent.com/jonasleft/dotfiles/master/bootstrap.sh | bash
+
+sudo apt install git
 git clone --recursive https://github.com/jonasleft/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
 ./setup_dotfiles.sh
